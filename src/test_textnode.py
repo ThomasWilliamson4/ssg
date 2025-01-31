77bdc9e1cef9c9972cb7a180b405825fde4df25d
BvWaffles <tomwilliamson4@gmail.com>
2025-01-31 14:36:01 +1030

    added test cases

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,6 +8,27 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+    
+    def test_not_equal(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.CODE)
+        self.assertNotEqual(node, node2)
+    
+    def test_none_url(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        self.assertIsNone(node.url)
+    
+    def test_different_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("Different text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_empty_string(self):
+        node = TextNode("", TextType.IMAGE)
+        node2 = TextNode("", TextType.IMAGE)
+        self.assertEqual(node, node2)
+
+    
 
 
 if __name__ == "__main__":
