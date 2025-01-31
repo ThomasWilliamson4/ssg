a3dc4aae4349f54a4b64813b2b1f3bf5cc11b082
BvWaffles <tomwilliamson4@gmail.com>
2025-01-31 22:41:08 +1030

    post chapter check and update

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -32,11 +32,29 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.NORMAL, "https://www.boot.dev")
         self.assertEqual("TextNode(This is a text node, normal, https://www.boot.dev)", repr(node))
 
-    def test_text_to_node(self):
-        node = TextNode("This is a link node", TextType.IMAGE, "https://www.boot.dev")
-        self.assertEqual(node.text_node_to_html_node(),'<img src="https://www.boot.dev" alt="This is a link node"></img>')
 
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.NORMAL)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+
+    def test_bold(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -21,30 +21,30 @@ class TextNode():
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
     
-    def text_node_to_html_node(text_node):
-        match text_node.text_type:
-            case TextType.NORMAL:
-                new_leaf = LeafNode(None, text_node.text)
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.NORMAL:
+            new_leaf = LeafNode(None, text_node.text)
                 
-            case TextType.BOLD:
+        case TextType.BOLD:
                 new_leaf = LeafNode("b",text_node.text)
 
-            case TextType.ITALIC:
-                new_leaf = LeafNode("i", text_node.text)
+        case TextType.ITALIC:
+            new_leaf = LeafNode("i", text_node.text)
                 
-            case TextType.CODE:
-                new_leaf =  LeafNode("code", text_node.text)
+        case TextType.CODE:
+            new_leaf =  LeafNode("code", text_node.text)
 
-            case TextType.LINK:
-                 new_leaf =  LeafNode("a", text_node.text, {"href" : text_node.url})
+        case TextType.LINK:
+             new_leaf =  LeafNode("a", text_node.text, {"href" : text_node.url})
 
-            case TextType.IMAGE:
-                new_leaf = LeafNode(tag ="img", value = "", props ={"src": text_node.url, "alt": text_node.text})
+        case TextType.IMAGE:
+            new_leaf = LeafNode(tag ="img", value = "", props ={"src": text_node.url, "alt": text_node.text})
 
-            case _:
-                raise Exception("no text type") 
+        case _:
+            raise Exception("no text type") 
 
-        return new_leaf.to_html()   
+    return new_leaf   
 
 
     
