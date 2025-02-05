b475ed6c46822758cc3b2083b56cc987381f2f4e
BvWaffles <tomwilliamson4@gmail.com>
2025-02-05 16:15:07 +1030

    Restructure after lesson complete with blocks in their own .py

---
+++ src/markdown_blocks.py
@@ -0,0 +1,10 @@
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    stripped_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        stripped_blocks.append(block)
+ 
+    return stripped_blocks
