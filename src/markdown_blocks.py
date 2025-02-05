0c9f4f5051f81526ae3424893ff713523fb405aa
BvWaffles <tomwilliamson4@gmail.com>
2025-02-05 18:00:40 +1030

    CH4 L3

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,8 @@
+import re
+from htmlnode import HTMLNode
+from 
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     stripped_blocks = []
@@ -8,3 +13,67 @@ def markdown_to_blocks(markdown):
         stripped_blocks.append(block)
  
     return stripped_blocks
+
+def block_to_block_type(block):
+    if re.match(r'^#{1,6}\s+\w', block):
+        return "heading"
+    if block.startswith("```") and block.endswith("```"):
+        return "code"
+    lines = block.split("\n")
+    if all(line.startswith(">") for line in lines):
+        return "quote"
+    if all(line.startswith(("* ","- ")) for line in lines):
+        return "unordered list"
+    for i in range(len(lines)):
+        number = f"{i + 1}. "
+        if not lines[i].startswith(number):
+            break
+    else: 
+        return "ordered list"
+   
+    return "paragraph"
+
+def text_to_children(text):
+
+
+def convert_paragraph_block(text):
+
+def convert_heading_block(text):
+
+def convert_code_block(text):
+
+def convert_quote_block(text):
+
+def convert_unordered_list_block(text):
+
+def convert_ordered_list_block(text):
+
+
+
+
+def block_to_html(block):
+    type = block_to_block_type(block)
+    if type == "code":
+        return convert_code_block(block)
+    if type == "quote":
+        return convert_quote_block(block)
+    if type == "unordered list":
+        return convert_unordered_list_block(block)
+    if type == "ordered list":
+        return convert_ordered_list_block(block)
+    if type == "heading":
+        return convert_heading_block(block)
+    if type == "paragraph":
+        return convert_paragraph_block(block)
+    raise ValueError("invalid block type")
+
+def markdown_to_html(markdown):
+    blocks = markdown_to_blocks(markdown)
+  
+
+        
+
+
+
+        
+
